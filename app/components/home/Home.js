d37d2d3b99b75b030e82ddc845b3cfe7e30c55e2
Matthew Sembinelli <matthew.sembinelli@gmail.com>
2018-02-19 20:37:11 -0700

    Add stats card (code needs refactoring)
    TODO: Refactor to use database fetches instead of
    manually doing querys with map, filter and reduce

--- app/components/home/Home.js
+++ app/components/home/Home.js
@@ -10,7 +10,8 @@ import {
   TableRow,
   TableRowColumn,
 } from 'material-ui/Table'
-// import Card from 'material-ui/Card'
+import { Card, CardHeader } from 'material-ui/Card'
+import styles from '../../styles/home.scss'
 
 class Home extends Component {
   constructor(props) {
@@ -50,9 +51,90 @@ class Home extends Component {
         <div>Loading...</div>
       )
     }
+    const wins = entryList.filter( entry => {
+        return entry.win === 'Yes'
+    })
+
+    // Beware really crappy code...has to be a better way to do this (probably use a database fetch)
+    // Most common winning sizes
+    const sizeSmall = entryList.filter( entry => {
+      if ( entry.win === 'Yes') {
+        return entry.size === 'S'
+      }
+    })
+
+    const sizeMedium = entryList.filter( entry => {
+      if ( entry.win === 'Yes') {
+        return entry.size === 'M'
+      }
+    })
+
+    const sizeLarge = entryList.filter( entry => {
+      if ( entry.win === 'Yes') {
+        return entry.size === 'L'
+      }
+    })
+
+    const sizeXLarge = entryList.filter( entry => {
+      if ( entry.win === 'Yes') {
+        return entry.size === 'XL'
+      }
+    })
+
+    const sizeApp = entryList.filter( entry => {
+      if ( entry.win === 'Yes') {
+        return entry.size === 'App'
+      }
+    })
+
+    const sizeWins = [ { size: 'S', wins: sizeSmall.length },
+                      { size: 'M', wins: sizeMedium.length },
+                      { size: 'L', wins: sizeLarge.length },
+                      { size: 'XL', wins: sizeXLarge.length },
+                      { size: 'App', wins: sizeApp.length } ]
+    
+    const mostWins = sizeWins.reduce((max, p) => p.wins > max ? p.wins : max, sizeWins[0].wins)
+    let sizeslabel = ''
+    sizeWins.filter( entry => {
+      if ( entry.wins === mostWins ) {
+        if( entry.size ) {
+          sizeslabel += ( entry.size + ', ' )
+        }
+      }
+    })
+
+    // Most purchases
+    // const topPurchaser = entryList.filter( entry => {
+    //   if ( entry.win === 'Yes') {
+    //     return entry.size === 'App'
+    //   }
+    // })
+
+    const totalWinsText = `Total Wins: ${ wins.length }`
+    const winRate = `Win Rate: ${ wins.length / entryList.length }`
+    const mostCommonWinningSize = `Most Common Winning Sizes: ${ sizeslabel }`
+    const mostPurchases = 'Most Purchases: '
 
     return (
       <div>
+        <div className={ styles.stats }>
+          <Card expanded={ true }>
+            <ul>
+              <li>
+                <h3><CardHeader>{ totalWinsText }</CardHeader></h3>
+              </li>
+              <li>
+                <h3><CardHeader>{ winRate }</CardHeader></h3>
+              </li>
+              <li>
+                <h3><CardHeader>{ mostCommonWinningSize }</CardHeader></h3>
+              </li>
+              <li>
+                <h3><CardHeader>{ mostPurchases }</CardHeader></h3>
+              </li>
+            </ul>
+          </Card>
+        </div>
         <div>
           <Table>
             <TableHeader displaySelectAll={ false } adjustForCheckbox={ false }>

cf18f1c6fd96816ec5169eb69c633cf5fe16409e
Matthew Sembinelli <matthew.sembinelli@gmail.com>
2018-02-25 21:53:56 -0700

    Add nonfunctional uploader using react dropzone

--- app/components/new/AddCsv.js
+++ app/components/new/AddCsv.js
@@ -5,48 +5,98 @@ import { bindActionCreators } from 'redux'
 import { reduxForm, Field } from 'redux-form'
 import { sendEntryCsv } from '../../actions/entries'
 import { connect } from 'react-redux'
+import Folder from 'material-ui/svg-icons/file/folder'
 
-const renderField = ({
-  input,
-  type,
-  placeholder,
-  meta: { touched, error },
-}) => (
-  <div
-    className={ `${styles.inputgroup} ${
-      touched && error ? styles.haserror : ''
-    }` }
-  >
-    <h4>{ placeholder }</h4>
-    <input type={ type } placeholder={ placeholder } { ...input } />
-    { touched && error && <div className={ styles.formerror }>{ error }</div> }
-  </div>
-)
+import Dropzone from 'react-dropzone'
+
+const FILE_FIELD_NAME = 'files'
+
+const dropzoneStyles = {
+  rejected: {
+    borderStyle: 'solid',
+    borderColor: '#e15258',
+    backgroundColor: '#eee',
+  },
+  disabled: {
+    opacity: 0.5,
+  },
+  active: {
+    borderStyle: 'solid',
+    borderColor: '#28cb75',
+    backgroundColor: '#eee',
+  },
+  default: {
+    width: '100%',
+    height: 300,
+    borderRadius: 5,
+    borderStyle: 'solid',
+    borderColor: '#c0c4c6',
+  },
+}
+
+const iconStyles = {
+  margin: 'auto',
+  height: '64px',
+  width: '64px',
+  color: '#4c5760',
+}
+
+const renderDropzoneInput = field => {
+  const [ file ] = field.input.value
+  return (
+    <div>
+      <Dropzone
+        accept="application/vnd.ms-excel"
+        name={ field.name }
+        onDrop={ field.input.onChange }
+        style={ dropzoneStyles.default }
+        activeStyle={ dropzoneStyles.active }
+        rejectStyle={ dropzoneStyles.rejected }
+      >
+        <div className={ styles.filegroupicon }>
+          <Folder style={ iconStyles } />
+        </div>
+        { !file && <h2>Drag and drop a CSV, or click here to browse</h2> }
+        <h2>{ file && file.name }</h2>
+      </Dropzone>
+      { field.meta.touched &&
+        field.meta.error && <span className="error">{ field.meta.error }</span> }
+    </div>
+  )
+}
 
 class AddCsv extends Component {
   constructor(props) {
     super(props)
 
     this.handleFormSubmit = this.handleFormSubmit.bind(this)
+    this.onChange = this.onChange.bind(this)
   }
 
   handleFormSubmit(formProps) {
-    this.props.sendEntryCsv(formProps)
+    if (
+      Object.keys(formProps).length !== 0 &&
+      formProps.constructor !== Object
+    ) {
+      this.props.sendEntryCsv(formProps)
+    }
+    console.log(formProps)
+  }
+
+  onChange(props) {
+    console.log(props)
   }
 
   render() {
-    const { handleSubmit } = this.props
+    const { handleSubmit, reset } = this.props
 
     return (
       <div className={ styles.formcontainer }>
-        <h2>Upload a CSV...</h2>
         <form onSubmit={ handleSubmit(this.handleFormSubmit) }>
-          { /* File */ }
           <Field
-            name="file"
-            component={ renderField }
-            type="text"
-            placeholder=""
+            name={ FILE_FIELD_NAME }
+            component={ renderDropzoneInput }
+            onChange={ this.onChange }
           />
 
           { /* Server error message */ }
@@ -62,6 +112,11 @@ class AddCsv extends Component {
           { /* Submit button */ }
           <button type="submit" className={ styles.btn }>
             Upload
+          </button>
+
+          { /* Reset button */ }
+          <button onClick={ reset } className={ styles.altbtn }>
+            Clear
           </button>
         </form>
       </div>
